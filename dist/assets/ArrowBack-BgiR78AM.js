c39f290e14ea8bbb3589795c0441ebe497c4c706
root <root@vmi3385559.contaboserver.net>
2026-06-23 14:33:46 +0200

    Build Tue Jun 23 14:33:46 CEST 2026

---
+++ dist/assets/ArrowBack-BgiR78AM.js
@@ -0,0 +1,2 @@
+import{i as e}from"./mui-qANsYBmx.js";import{r as t}from"./index-B2duzkA5.js";import{j as a}from"./query-CnpBhocU.js";var r={},o=e;Object.defineProperty(r,"__esModule",{value:!0});var u=r.default=void 0,i=o(t()),f=a;u=r.default=(0,i.default)((0,f.jsx)("path",{d:"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20z"}),"ArrowBack");export{u as d};
+//# sourceMappingURL=ArrowBack-BgiR78AM.js.map
